e95f1050b22cd492674327c91482629546a2685d
movenpickdev <nayot.tientong@gmail.com>
2024-06-24 16:15:23 +0700

    change mac address

--- section2_macAddressChanger/main_function.py
+++ section2_macAddressChanger/main_function.py
@@ -9,7 +9,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return  parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[+] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[+] Please specify a new mac, use --help for more info.")
+    return options
 
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address for {interface} interface to {new_mac}")
@@ -17,5 +22,13 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
+
+
+
+
+
+
+
+
